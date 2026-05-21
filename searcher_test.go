ce21a372f2cf7c6ddb27fb653cc68f4010a32daf
NikWork <nik8021@gmail.com>
2026-05-21 14:04:46 +0300

    fix: change , to ;

--- searcher_test.go
+++ searcher_test.go
@@ -6,7 +6,7 @@ func TestGetEmail(t *testing.T) {
 	htmlBody := "<a href=\"mailto:test@domen.test\">Send email</a>:"
 	arrayEmails := getEmail(htmlBody)
 	if len(arrayEmails) != 1 {
-		t.Errorf("len(arrayEmails) = %d, want 1\n", len(arrayEmails))
+		t.Errorf("len(arrayEmails) = %d; want 1\n", len(arrayEmails))
 	}
 	if arrayEmails[0] != "test@domen.test" {
 		t.Errorf("arrayEmails[0] = %s; want test@domen.test\n", arrayEmails[0])
@@ -17,7 +17,7 @@ func TestGetEmail_BrokenEmail(t *testing.T) {
 	htmlBody := "test@domen.t"
 	arrayEmails := getEmail(htmlBody)
 	if len(arrayEmails) != 0 {
-		t.Errorf("len(arrayEmails) = %d, want 0\n", len(arrayEmails))
+		t.Errorf("len(arrayEmails) = %d; want 0\n", len(arrayEmails))
 	}
 }
 
@@ -25,7 +25,7 @@ func TestClearCoincidencesEmails(t *testing.T) {
 	arrayEmails := []string{"test@domen.test", "test@domen.test", "1@1.1", "1@1.1"}
 	arrayEmails = clearCoincidencesEmails(arrayEmails)
 	if len(arrayEmails) != 2 {
-		t.Errorf("len(arrayEmails) = %d, want 2\n", len(arrayEmails))
+		t.Errorf("len(arrayEmails) = %d; want 2\n", len(arrayEmails))
 	}
 	emailTest := false
 	emailOne := false
