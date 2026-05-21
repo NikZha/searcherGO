6c930709a5c4b735a0279aadde6b5d2325d8ae1a
NikWork <nik8021@gmail.com>
2026-05-21 12:44:32 +0300

    feat: tests for func clearCoincidencesEmails

--- searcher_test.go
+++ searcher_test.go
@@ -12,3 +12,17 @@ func TestGetEmail(t *testing.T) {
 		t.Errorf("arrayEmails[0] = %s; want test@domen.test", arrayEmails[0])
 	}
 }
+
+func TestClearCoincidencesEmails(t *testing.T) {
+	arrayEmails := []string{"test@domen.test", "test@domen.test", "1@1.1", "1@1.1"}
+	arrayEmails = clearCoincidencesEmails(arrayEmails)
+	if len(arrayEmails) != 2 {
+		t.Errorf("len(arrayEmails) = %d, want 2", len(arrayEmails))
+	}
+	if arrayEmails[0] != "test@domen.test" {
+		t.Errorf("arrayEmails[0] = %s; want test@domen.test", arrayEmails[0])
+	}
+	if arrayEmails[1] != "1@1.1" {
+		t.Errorf("arrayEmails[1] = %s; want 1@1.1", arrayEmails[1])
+	}
+}
